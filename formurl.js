b0f7bdd2e378f5e1c8f1d2216a090732c4b1de72
developerapr <44453922+developerapr@users.noreply.github.com>
2022-01-20 08:59:52 +0530

    Update formurl.js

--- formurl.js
+++ formurl.js
@@ -2,10 +2,13 @@ $("#was_the_finance_arranged_through_a_broker").on('change', function() {
  
   switch ($(this).val()) {      
     case 'Yes':
-       window.module.lp.form.data.url = "http://www.google.com";
+       window.module.lp.form.data.url = "http://unbouncepages.com/firths-law-s2/";
+        break;
+     case 'Unsure':
+        window.module.lp.form.data.url = "http://unbouncepages.com/firths-law-s1/";
         break;
     case 'No':
-        window.module.lp.form.data.url = "http://www.bing.com";
+        window.module.lp.form.data.url = "http://unbouncepages.com/firths-law-s1/";
         break;
   }
   
